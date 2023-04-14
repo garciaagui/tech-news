2edfc2e2448d1c8bd87672381969981f09e80e11
Guilherme Garcia <garciaguig@gmail.com>
2023-04-14 15:36:20 -0300

    feat: Implements menu functions at `menu.py`

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,5 +1,14 @@
 import sys
 
+from .scraper import get_tech_news
+
+from .analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_category,
+)
+
+from .analyzer.ratings import top_5_categories
 
 # def print_menu():
 #     menu_options = {
@@ -16,21 +25,49 @@ import sys
 #         print(menu_options[key])
 
 
-def get_function_by_selected_option(option: int):
+def initialize_get_tech_news():
+    amount = int(input("Digite quantas notícias serão buscadas: "))
+    get_tech_news(amount)
+
+
+def initialize_search_by_title():
+    title = str(input("Digite o título: "))
+    return search_by_title(title)
+
+
+def initialize_search_by_date():
+    date = str(input("Digite a data no formato aaaa-mm-dd: "))
+    return search_by_date(date)
+
+
+def initialize_search_by_category():
+    category = str(input("Digite a categoria: "))
+    return search_by_category(category)
+
+
+def initialize_top_5_categories():
+    return top_5_categories()
+
+
+def exit():
+    return "Encerrando script"
+
+
+def get_function_by_option(option: int):
     func_options = {
-        0: "Digite quantas notícias serão buscadas:",
-        1: "Digite o título:",
-        2: "Digite a data no formato aaaa-mm-dd:",
-        3: "Digite a categoria:",
-        4: "Listando top 5 categorias...",
-        5: "Saindo...",
+        0: initialize_get_tech_news,
+        1: initialize_search_by_title,
+        2: initialize_search_by_date,
+        3: initialize_search_by_category,
+        4: initialize_top_5_categories,
+        5: exit,
     }
 
     if option not in func_options:
         raise KeyError()
 
     else:
-        print(func_options[option])
+        print(func_options[option]())
 
 
 def analyzer_menu():
@@ -46,7 +83,7 @@ def analyzer_menu():
 
     try:
         selected_option = int(input())
-        get_function_by_selected_option(selected_option)
+        get_function_by_option(selected_option)
 
     except (ValueError, KeyError):
-        sys.stderr.write("Opção inválida")
+        sys.stderr.write("Opção inválida\n")

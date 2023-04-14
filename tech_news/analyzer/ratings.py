d8824aaa7d2618d49ba8c2b28e8a9568a976778a
Guilherme Garcia <garciaguig@gmail.com>
2023-04-14 14:14:24 -0300

    feat: Implements `top_5_categories` at `ratings.py`

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,26 @@
-# Requisito 10
+from ..database import find_news
+from .search_engine import search_by_category
+
+
+def get_categories():
+    categories = set()
+
+    for news in find_news():
+        categories.add(news["category"])
+
+    return categories
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = get_categories()
+    ratings = list()
+
+    for category in categories:
+        category_counter = tuple((category, len(search_by_category(category))))
+        ratings.append(category_counter)
+
+    ratings.sort(key=lambda a: (-a[1], a[0]))
+    # -a[1] -> 1o criteria - Sort category counter in descending order
+    # a[0] -> 2o criteria - Sort category name in ascending order
+
+    return [rating[0] for rating in ratings[:5]]
